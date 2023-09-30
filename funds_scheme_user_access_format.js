210b639f3b5feb31777034d32e0130c8a88fd790
Vishal Gupta <63388692+vishalguptax@users.noreply.github.com>
2023-09-30 09:48:54 +0530

    Create funds_scheme_user_access_format.js

---
+++ funds_scheme_user_access_format.js
@@ -0,0 +1,98 @@
+// funds and scheme user access payload and response 
+
+
+const fundsSchemeList = [
+    {
+        id: 2896,
+        fundName: 'Anguilla Fund - 4792',
+        schemes: [
+            {
+                id: 3785,
+                schemeName: 'Newton Scheme - 126',
+                enabled: true,
+            },
+            {
+                id: 442,
+                schemeName: 'Vaughn Scheme - 917',
+                enabled: true,
+            },
+            {
+                id: 6811,
+                schemeName: 'Daniels Scheme - 844',
+                enabled: true,
+            },
+            {
+                id: 818,
+                schemeName: 'Todd Scheme - 151',
+                enabled: true,
+            },
+            {
+                id: 3915,
+                schemeName: 'Murphy Scheme - 809',
+                enabled: true,
+            },
+        ],
+    },
+    {
+        id: 2094,
+        fundName: 'St. Vincent & Grenadines Fund - 6676',
+        schemes: [
+            {
+                id: 5905,
+                schemeName: 'Becker Scheme - 349',
+                enabled: true,
+            },
+            {
+                id: 2979,
+                schemeName: 'Cobb Scheme - 195',
+                enabled: true,
+            },
+            {
+                id: 1389,
+                schemeName: 'Vasquez Scheme - 793',
+                enabled: true,
+            },
+            {
+                id: 1667,
+                schemeName: 'Holland Scheme - 869',
+                enabled: true,
+            },
+            {
+                id: 1779,
+                schemeName: 'Vargas Scheme - 741',
+                enabled: true,
+            },
+        ],
+    },
+    {
+        id: 448,
+        fundName: 'Mauritius Fund - 4162',
+        schemes: [
+            {
+                id: 4897,
+                schemeName: 'Payne Scheme - 160',
+                enabled: true,
+            },
+            {
+                id: 3552,
+                schemeName: 'Chapman Scheme - 952',
+                enabled: true,
+            },
+            {
+                id: 2804,
+                schemeName: 'Boyd Scheme - 479',
+                enabled: true,
+            },
+            {
+                id: 4815,
+                schemeName: 'James Scheme - 264',
+                enabled: true,
+            },
+            {
+                id: 5760,
+                schemeName: 'Hubbard Scheme - 399',
+                enabled: true,
+            },
+        ],
+    },
+]
